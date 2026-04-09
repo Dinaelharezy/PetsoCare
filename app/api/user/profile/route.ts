ea4d3bdf2e3fe4191a416af535f407f5bdd4d00a
Dina_ Elharezy <delharezy@gmail.com>
2026-04-09 17:41:14 +0200

    feat: added authentication and edit profile

--- app/api/user/profile/route.ts
+++ app/api/user/profile/route.ts
@@ -17,3 +17,24 @@ export async function GET() {
     role:  user.role   ?? "User",
   })
 }
+
+// export async function GET() {
+//   const session = await auth()
+
+//   if (!session?.user) {
+//     return NextResponse.json({ message: "Unauthenticated" }, { status: 401 })
+//   }
+
+//   const token = session.user.accessToken
+
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/profile`, {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//     cache: "no-store"
+//   })
+
+//   const data = await res.json()
+
+//   return NextResponse.json(data)
+// }
