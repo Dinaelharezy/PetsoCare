ebc93c3ba5663dc233b6cbfedd551a980c1f322e
Dina_ Elharezy <delharezy@gmail.com>
2026-06-21 02:57:02 +0300

    refactor : changed structure of code

--- app/clinic/dashboard/page.tsx
+++ app/clinic/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import ClinicDashboardClient from "@/components/Clinics/ClinicDashboardClient";
+import ClinicDashboardClient from "@/components/Clinics/pages/ClinicDashboardClient";
 
 export default function ClinicDashboardPage() {
   return (
